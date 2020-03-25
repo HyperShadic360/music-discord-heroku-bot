0095641cd1ddb783cb5f870304547b61eb203f87
Bowen Zhang <bowzotang@gmail.com>
2020-03-24 20:00:29 -0400

    Update

--- bot.js
+++ bot.js
@@ -14,7 +14,14 @@ bot.music.start(bot, {
     // Set the api key used for YouTube.
     // This is required to run the bot.
     youtubeKey: process.env.BOT_YOUTUBE_TOKEN,
-    botPrefix: PREFIX
+    botPrefix: PREFIX,
+
+    maxQueueSize: 0,
+    anyoneCanSkip: true,
+    messageHelp: true,
+    anyoneCanAdjust: true,
+    anyoneCanLeave: true
+
 });
 
 // Events.
