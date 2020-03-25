578c2b989ab479c2c225143cfc92d182024f981e
Bowen Zhang <bowzotang@gmail.com>
2020-03-25 10:59:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ bot.music.start(bot, {
 
 // Events.
 bot.on("ready", function() {
-    bot.user.setActivity(`Hi, Im The Music Bot`);
+    bot.user.setActivity(`music`);
     console.log(`${bot.user.username} is Ready!`);
 });
 
